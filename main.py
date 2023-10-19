f9de82d5a703f208b3ea9c812727310f230d1850
caleb765landis <caleb765landis@gmail.com>
2023-10-18 19:20:42 -0600

    Working player camera

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from settings import *
 from ShaderProgram import ShaderProgram
 from Scene import Scene
+from Player import Player
 import moderngl as mgl
 import pygame as pg
 import sys
@@ -29,12 +30,21 @@ class VoxelEngine:
         self.delta_time = 0
         self.time = 0
 
+        # lock mouse pointer within window
+        pg.mouse.set_visible(False)
+        pg.event.set_grab(True)
+        # set its position to center of window
+        pg.mouse.set_pos([WIN_RES.x / 2, WIN_RES.y / 2])
+
         # attribute for checking if engine is running
         self.is_running = True
 
         self.on_init()
 
     def on_init(self):
+        # intitialize player
+        self.player = Player(self)
+        
         # initialize shader program
         self.shader_program = ShaderProgram(self)
 
@@ -42,6 +52,9 @@ class VoxelEngine:
         self.scene = Scene(self)
 
     def update(self):
+        # update player
+        self.player.update()
+
         # update shaders
         self.shader_program.update()
 
